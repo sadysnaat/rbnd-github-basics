9321755d651cb6da9ec4c60874dd860741141a33
Deepak Sharma <deepaksharma.n@flipkart.com>
2016-02-20 12:53:44 +0530

    Print ASCII Art puppy

---
+++ puppies.rb
@@ -0,0 +1,10 @@
+#prints out a cute puppy
+
+puts "   _=,_"
+puts "o_/6 /#\\"
+puts "\\__ |##/"
+puts " ='|--\\"
+puts "   /   #'-."
+puts "   \\#|_   _'-. /"
+puts "    |/ \\_( # |\" "
+puts "   c/ ,--___/"
